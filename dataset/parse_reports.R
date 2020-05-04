96e5b561696e69c2aa6bb777a165831cef012bd8
Harpo MAxx <harpomaxx@gmail.com>
2020-05-04 18:24:21 -0300

    add script for parsing COVID19 reports from Argentina Health Minister

---
+++ dataset/parse_reports.R
@@ -0,0 +1,82 @@
+# 
+library(pdftools)
+library(stringr)
+library(pdftools)
+library(stringr)
+library(lubridate)
+library(dplyr)
+library(readr)
+
+dir_reportes<-"/home/harpo/informes"
+files <- list.files(path = dir_reportes, pattern = "pdf$")
+report<-data.frame()
+
+for(inf_file in files){
+  print(inf_file)
+  
+  inf<-pdf_text(paste0(dir_reportes,"/",inf_file))
+  inf<-str_split(inf,'\n')
+  provincias=c()
+  
+  total_importados<-0
+  total_contacto<-0
+  total_comunitario<-0
+  total_descartados<-0
+  
+  for (line in unlist(inf)){
+    line<-gsub("|•","-",line)
+  
+      if (grepl("^.* hasta ayer es de (\\d+\\.?\\d+) .*$",line,perl=T))
+      total_descartados<-gsub("^.* hasta ayer es de (\\d+\\.?\\d+) .*$","\\1",line,perl=T)
+    
+    if (grepl("^.*[ ]*\\((\\d+\\,?.?\\d*)%\\) son importados,.*$",line,perl=T))
+      total_importados<-gsub("^.*[ ]*\\((\\d+\\,?.?\\d*)%\\) son importados,.*$","\\1", line,perl=T)
+    
+    if (grepl("^.*[ ]*\\((\\d+\\,?.?\\d*)%\\) son contactos.*$",line,perl=T))
+      total_contacto<-gsub("^.*[ ]*\\((\\d+\\,?.?\\d*)%\\) son contactos.*$","\\1", line,perl=T)
+    
+    if (grepl("^.*[ ]*\\((\\d+\\,?.?\\d*)%\\) son casos de circu.*$",line,perl=T))
+      total_comunitario<-gsub("^.*[ ]*\\((\\d+\\,?.?\\d*)%\\) son casos de circu.*$","\\1", line,perl=T)
+    
+    if (grepl("^.* es de (\\d+\\.?\\d+), .*$",line,perl=T))
+      total_infectados<-gsub("^.* es de (\\d+\\.?\\d+), .*$","\\1",line,perl=T)
+    
+    if (grepl("^.* (\\d+\\.?\\d+) fallecieron.*$",line,perl=T))
+      total_fallecidos<-gsub("^.* (\\d+\\.?\\d+) fallecieron.*$","\\1",line,perl=T)
+    
+    if (grepl("^.*altas es de (\\d+\\.?\\d+) .*$",line,perl=T))
+      total_recuperados<-gsub("^.*altas es de (\\d+\\.?\\d+) .*$","\\1",line,perl=T)
+    
+    if (grepl("^.*confirmados (\\d+\\.?\\d+) .*$",line,perl=T))
+      total_casos_nuevos<-gsub("^.*confirmados (\\d+\\.?\\d+) .*$","\\1", line,perl=T)
+    
+    
+    #provincias
+    if (grepl("^[ ]*(-|•|)?[ ]*([a-zA-Z áéíóú]+) ((-)?\\d+\\.?\\d*)(\\*+)? (\\||\\/) .*$", line,perl=T)){
+      provincia<-gsub("^[ ]*(-|•|)?[ ]*([a-zA-Z áéíóú]+) ((-)?\\d+\\.?\\d*)(\\*+)? (\\||\\/) .*$","\\2|\\3" , line,perl=T) %>% str_split("\\|",simplify = T)
+      if (grepl(provincia[1],"Aires"))
+        provincia[1]<-"Buenos Aires"
+      provincias<-rbind(provincias,cbind(provincia=provincia[1],casos_nuevos=provincia[2]))
+      
+    }
+  }
+  
+  provincias<-t(provincias)
+  provincias_col_names<-t(provincias[1,])
+  casos_nuevos <-t(as.numeric(provincias[2,]))
+  provincias<-casos_nuevos
+  colnames(provincias)<-t(provincias_col_names)
+  provincias<-provincias %>% as.data.frame()
+  
+  new_report<-data.frame(file=inf_file,total_infectados=total_infectados,
+                         total_fallecidos=total_fallecidos,
+                         total_recuperados=total_recuperados,
+                         total_casos_nuevos=total_casos_nuevos,
+                         total_importados=total_importados,
+                         total_contacto=total_contacto,
+                         total_comunitario=total_comunitario,
+                         total_descartados=total_descartados)
+  new_report<-cbind(new_report,provincias)
+  report<-rbind(report,new_report)
+}
+report<- report %>% mutate(date=dmy(str_sub(file,1,8))-1) %>% arrange(date) %>% readr::write_csv(paste0(dir_reportes,"/covid19_argentina.csv"))
